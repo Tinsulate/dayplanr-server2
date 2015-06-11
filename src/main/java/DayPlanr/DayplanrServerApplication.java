6edf773130fa239370636104a44d0df7096a88a3
Tinsulate <siira.jussi@gmail.com>
2015-06-11 15:35:18 +0300

    Initial commit

---
+++ src/main/java/DayPlanr/DayplanrServerApplication.java
@@ -0,0 +1,12 @@
+package DayPlanr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayplanrServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayplanrServerApplication.class, args);
+    }
+}
